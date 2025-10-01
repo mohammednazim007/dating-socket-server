d4ad567780cbb9975a34fb922f1118371032e0d9
mohammednazim007 <mohammednazim3629@gmail.com>
2025-10-01 16:50:59 +0600

    fix: optimize the cloudinary image size

--- src/cloudinary/upload.ts
+++ src/cloudinary/upload.ts
@@ -8,9 +8,11 @@ const storage = new CloudinaryStorage({
     folder: "uploads", // Cloudinary folder
     allowed_formats: ["jpg", "png", "jpeg", "webp"],
     resource_type: "auto",
-    transformation: [{ width: 300, height: 300, crop: "limit" }],
-    quality: "auto",
-    fetch_format: "auto",
+    transformation: [
+      { width: 300, height: 300, crop: "limit" },
+      { quality: "auto" },
+      { fetch_format: "auto" },
+    ],
   } as {
     folder: string;
     allowed_formats: string[];
